34ffa701dcd2571e043250782cbfd02f6290a9c3
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-06-14 18:00:16 -0500

    Initial push
    N/A

---
+++ VX-API/Main.cpp
@@ -0,0 +1,33 @@
+#include "MalwareHeader.h"
+
+
+
+INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ INT nCmdShow)
+{
+	RtlInitializeNtdllFunctionsForForwards(TRUE);
+
+	IsDllLinkedToModule(SH_DJB2, 0, TRUE);
+
+	return 0;
+}
+
+/*
+#pragma comment(linker, "/entry:__EntryPointEx")
+#pragma comment(linker, "/subsystem:windows")
+
+INT __EntryPointEx(VOID)
+{
+	DWORD dwError = ERROR_SUCCESS;
+	BOOL bFlag = FALSE;
+
+
+	bFlag = TRUE;
+
+EXIT_ROUTINE:
+
+	if (!bFlag)
+		dwError = GetLastError();
+
+	return dwError;
+}
+*/
