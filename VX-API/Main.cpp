ed3d4eadd69b7e827b9439dffac5fe448219a191
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-06-17 03:16:41 -0500

    update
    0000001

--- VX-API/Main.cpp
+++ VX-API/Main.cpp
@@ -4,12 +4,12 @@
 
 INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ INT nCmdShow)
 {
-	RtlInitializeNtdllFunctionsForForwards(TRUE);
+	RtlInitializeNtdllFunctionsForForwards(TRUE, SH_DJB2);
 
 	IsDllLinkedToModule(SH_DJB2, 0, TRUE);
 
 	return 0;
-}
+} 
 
 /*
 #pragma comment(linker, "/entry:__EntryPointEx")
