fbfd8ed4d8952beee9d80443ac6671ed1692977e
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-07-14 22:23:56 -0500

    Create StringCompare.cpp

---
+++ String Manipulation/StringCompare.cpp
@@ -0,0 +1,21 @@
+INT StringCompareA(LPCSTR String1, LPCSTR String2)
+{
+	for (; *String1 == *String2; String1++, String2++)
+	{
+		if (*String1 == '\0')
+			return 0;
+	}
+
+	return ((*(LPCSTR)String1 < *(LPCSTR)String2) ? -1 : +1);
+}
+
+INT StringCompareW(LPCWSTR String1, LPCWSTR String2)
+{
+	for (; *String1 == *String2; String1++, String2++)
+	{
+		if (*String1 == '\0')
+			return 0;
+	}
+
+	return ((*(LPCWSTR)String1 < *(LPCWSTR)String2) ? -1 : +1);
+}
