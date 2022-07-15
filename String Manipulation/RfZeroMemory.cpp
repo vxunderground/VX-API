eff6c46a901c4b38a9729fd34d73b5f97295ee63
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-07-14 22:14:23 -0500

    Create RfZeroMemory.cpp

---
+++ String Manipulation/RfZeroMemory.cpp
@@ -0,0 +1,14 @@
+VOID RfZeroMemory(PVOID Destination, SIZE_T Size)
+{
+	PULONG Dest = (PULONG)Destination;
+	SIZE_T Count = Size / sizeof(ULONG);
+
+	while (Count > 0)
+	{
+		*Dest = 0;
+		Dest++;
+		Count--;
+	}
+
+	return;
+}
