556221a8e71a63ec9df60c08064e658be7636f4c
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-07-14 22:22:29 -0500

    Create RfSecureStringCopy.cpp

---
+++ String Manipulation/RfSecureStringCopy.cpp
@@ -0,0 +1,17 @@
+PCHAR RfSecureStringCopyA(PCHAR String1, LPCSTR String2, SIZE_T Size)
+{
+	PCHAR pChar = String1;
+
+	while (Size-- && (*String1++ = *String2++) != '\0');
+
+	return pChar;
+}
+
+PWCHAR RfSecureStringCopyW(PWCHAR String1, LPCWSTR String2, SIZE_T Size)
+{
+	PWCHAR pChar = String1;
+
+	while (Size-- && (*String1++ = *String2++) != '\0');
+
+	return pChar;
+}
