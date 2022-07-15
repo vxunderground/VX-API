a1371e219f7e98f6c23d95028aef8c3b0dd90929
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-07-14 22:17:30 -0500

    Create CaplockString.cpp

---
+++ String Manipulation/CaplockString.cpp
@@ -0,0 +1,25 @@
+PCHAR CaplockStringA(PCHAR Ptr)
+{
+	PCHAR sv = Ptr;
+	while (*sv != '\0')
+	{
+		if (*sv >= 'a' && *sv <= 'z')
+			*sv = *sv - ('a' - 'A');
+
+		sv++;
+	}
+	return Ptr;
+}
+
+PWCHAR CaplockStringW(PWCHAR Ptr)
+{
+	PWCHAR sv = Ptr;
+	while (*sv != '\0')
+	{
+		if (*sv >= 'a' && *sv <= 'z')
+			*sv = *sv - ('a' - 'A');
+
+		sv++;
+	}
+	return Ptr;
+}
