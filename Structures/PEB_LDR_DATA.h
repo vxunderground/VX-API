923132cea25cb95071207960fd65353ec31a5fef
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-07-14 23:24:58 -0500

    Create PEB_LDR_DATA.h

---
+++ Structures/PEB_LDR_DATA.h
@@ -0,0 +1,8 @@
+typedef struct _PEB_LDR_DATA {
+	ULONG                   Length;
+	ULONG                   Initialized;
+	PVOID                   SsHandle;
+	LIST_ENTRY              InLoadOrderModuleList;
+	LIST_ENTRY              InMemoryOrderModuleList;
+	LIST_ENTRY              InInitializationOrderModuleList;
+} PEB_LDR_DATA, * PPEB_LDR_DATA;
