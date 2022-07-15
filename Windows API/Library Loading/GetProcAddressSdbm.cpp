ad143c940f0f8811d5b345328c3d328bbd422d86
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-07-15 12:52:47 -0500

    Create GetProcAddressSdbm.cpp

---
+++ Windows API/Library Loading/GetProcAddressSdbm.cpp
@@ -0,0 +1,26 @@
+DWORD64 __stdcall GetProcAddressSdbm(DWORD64 ModuleBase, DWORD64 Hash)
+{
+	PBYTE pFunctionName;
+	PIMAGE_DOS_HEADER Dos;
+	PIMAGE_NT_HEADERS Nt;
+	PIMAGE_FILE_HEADER File;
+	PIMAGE_OPTIONAL_HEADER Optional;
+
+	RtlLoadPeHeaders(&Dos, &Nt, &File, &Optional, (PBYTE*)&ModuleBase);
+
+	IMAGE_EXPORT_DIRECTORY* ExportTable = (PIMAGE_EXPORT_DIRECTORY)(ModuleBase + Optional->DataDirectory[0].VirtualAddress);
+	PDWORD FunctionNameAddressArray = (PDWORD)((LPBYTE)ModuleBase + ExportTable->AddressOfNames);
+	PDWORD FunctionAddressArray = (PDWORD)((LPBYTE)ModuleBase + ExportTable->AddressOfFunctions);
+	PWORD FunctionOrdinalAddressArray = (PWORD)((LPBYTE)ModuleBase + ExportTable->AddressOfNameOrdinals);
+
+	for (DWORD dwX = 0; dwX < ExportTable->NumberOfNames; dwX++)
+	{
+		pFunctionName = FunctionNameAddressArray[dwX] + (PBYTE)ModuleBase;
+
+		DWORD dwFunctionHash = HashStringSdbmA((PCHAR)pFunctionName);
+		if (Hash == dwFunctionHash)
+			return ((DWORD64)ModuleBase + FunctionAddressArray[FunctionOrdinalAddressArray[dwX]]);
+	}
+
+	return 0;
+}
