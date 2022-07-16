b592b00b832c6bb66ce3b1a2e14ca77745fa060b
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-07-16 05:50:45 -0500

    Create AdfOpenProcessOnCsrss.cpp

---
+++ Anti-Debug/AdfOpenProcessOnCsrss.cpp
@@ -0,0 +1,24 @@
+BOOL AdfOpenProcessOnCsrss(VOID)
+{
+	typedef DWORD(WINAPI* CSRGETPROCESSID)(VOID);
+	HMODULE hNtdll = NULL;
+	CSRGETPROCESSID CsrGetProcessId = NULL;
+	HANDLE hCsrHandle = NULL;
+
+	hNtdll = RfGetModuleHandleW(L"ntdll.dll");
+	if (hNtdll == NULL)
+		return FALSE;
+
+	CsrGetProcessId = (CSRGETPROCESSID)RfGetProcAddressA((DWORD64)hNtdll, "CsrGetProcessId");
+	if (CsrGetProcessId == NULL)
+		return FALSE;
+
+	hCsrHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, CsrGetProcessId());
+	if (hCsrHandle == NULL)
+		return FALSE;
+
+	if (hCsrHandle)
+		CloseHandle(hCsrHandle);
+
+	return TRUE;
+}
