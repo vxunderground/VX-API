3b22461913163d55a4f5b58ecee3c41fb1336031
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-07-16 15:15:43 -0500

    Update and rename IsCreateProcessDebugEventCodeSet.cpp to AdIsCreateProcessDebugEventCodeSet.cpp

--- Anti-Debug/AdIsCreateProcessDebugEventCodeSet.cpp
+++ Anti-Debug/AdIsCreateProcessDebugEventCodeSet.cpp
@@ -12,7 +12,7 @@ run in the presence of a debugger.
 Credit: Checkpoint Research
 */
 
-BOOL IsCreateProcessDebugEventCodeSet(VOID)
+BOOL AdIsCreateProcessDebugEventCodeSet(VOID)
 {
 	WCHAR FilePath[MAX_PATH * sizeof(WCHAR)] = { 0 };
 	HANDLE hHandle = INVALID_HANDLE_VALUE;
