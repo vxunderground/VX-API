2d5baa5e732ac3c9861f9d508d4b1c9aeee13068
rad9800 <105589633+rad9800@users.noreply.github.com>
2022-07-17 19:45:27 +0100

    GetFileSize issue
    GetFileSize doesn't work for files bigger than 2GB

--- Windows API/RfGetFileSizeFromPathDisposeHandle.cpp
+++ Windows API/RfGetFileSizeFromPathDisposeHandle.cpp
@@ -1,18 +1,20 @@
-DWORD RfGetFileSizeFromPathDisposeHandleW(PWCHAR Path, DWORD dwFlagsAndAttributes)
+LONGLONG RfGetFileSizeFromPathDisposeHandleW(PWCHAR Path, DWORD dwFlagsAndAttributes)
 {
-	DWORD Size = ERROR_SUCCESS;
+	LARGE_INTEGER LargeInteger;
 	HANDLE hHandle = INVALID_HANDLE_VALUE;
 		
 	hHandle = CreateFileW(Path, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, dwFlagsAndAttributes, NULL);
 	if (hHandle == INVALID_HANDLE_VALUE)
 		return INVALID_FILE_SIZE;
 
-	Size = GetFileSize(hHandle, NULL);
+	if (GetFileSizeEx(hHandle, &LargeInteger))
+	{
+		if (hHandle)
+			CloseHandle(hHandle);
+		return LargeInteger.QuadPart;
+	}
 	
-	if (hHandle)
-		CloseHandle(hHandle);
-
-	return Size;
+	return INVALID_FILE_SIZE;
 }
 
 DWORD RfGetFileSizeFromPathDisposeHandleA(PCHAR Path, DWORD dwFlagsAndAttributes)
