c9a90cbf8064fd8f65cb53ab92ffe546dda2d815
vxunderground <57078196+vxunderground@users.noreply.github.com>
2022-07-17 08:37:35 -0500

    Create CfCreateLocalAppDataObjectPath.cpp

---
+++ Windows API/CfCreateLocalAppDataObjectPath.cpp
@@ -0,0 +1,46 @@
+/*
+
+pBuffer == OUT
+Path == concatted, must have \\ in front i.e. L"\\File.exe"
+
+Credit: smelly__vx
+*/
+BOOL CfCreateLocalAppDataObjectPathW(PWCHAR pBuffer, PWCHAR Path, DWORD Size, BOOL bDoesObjectExist)
+{
+	if (pBuffer == NULL)
+		return FALSE;
+
+	if (RfGetEnvironmentVariableW(L"LOCALAPPDATA", pBuffer, Size) == 0)
+		return FALSE;
+
+	if (StringConcatW(pBuffer, Path) == 0)
+		return FALSE;
+
+	if (bDoesObjectExist)
+	{
+		if (!IsPathValidW(pBuffer))
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+BOOL CfCreateLocalAppDataObjectPathA(PCHAR pBuffer, PCHAR Path, DWORD Size, BOOL bDoesObjectExist)
+{
+	if (pBuffer == NULL)
+		return FALSE;
+
+	if (RfGetEnvironmentVariableA("LOCALAPPDATA", pBuffer, Size) == 0)
+		return FALSE;
+
+	if (StringConcatA(pBuffer, Path) == 0)
+		return FALSE;
+
+	if (bDoesObjectExist)
+	{
+		if (!IsPathValidA(pBuffer))
+			return FALSE;
+	}
+
+	return TRUE;
+}
