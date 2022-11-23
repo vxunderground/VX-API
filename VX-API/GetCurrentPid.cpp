490a6c2a54ccfb539ab51e0a94df3d12fbeb4bd4
RistBS <75935486+RistBS@users.noreply.github.com>
2022-11-23 19:33:31 +0100

    Create GetCurrentPid function

---
+++ VX-API/GetCurrentPid.cpp
@@ -0,0 +1,10 @@
+#include "Win32Helper.h"
+
+DWORD GetCurrentPid(VOID)
+{
+#if defined(_WIN64)
+    return (UINT32)__readgsqword(0x40);
+#elif define(_WIN32)
+    return (UINT32)__readfsdword(0x20);
+#endif
+}
