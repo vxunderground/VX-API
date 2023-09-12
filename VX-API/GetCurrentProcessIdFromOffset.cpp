13e7f0bddca7a8c6d6ca2c8aaf88516222f592e9
Josh Abraham <jabra@spl0it.org>
2023-09-12 15:19:22 -0400

    Update GetCurrentProcessIdFromOffset.cpp

--- VX-API/GetCurrentProcessIdFromOffset.cpp
+++ VX-API/GetCurrentProcessIdFromOffset.cpp
@@ -4,7 +4,7 @@ DWORD GetCurrentProcessIdFromOffset(VOID)
 {
 #if defined(_WIN64)
     return (UINT32)__readgsqword(0x40);
-#elif define(_WIN32)
+#elif defined(_WIN32)
     return (UINT32)__readfsdword(0x20);
 #endif
 }
